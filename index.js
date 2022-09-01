99a02e6ea3c07c183b97a7658f4f1084bb3ca728
Neeraj Dalal <admin@nrjdalal.com>
2022-09-01 20:19:26 +0530

    Thu Sep  1 20:19:26 IST 2022

---
+++ index.js
@@ -0,0 +1,37 @@
+const puppeteer = require('puppeteer')
+
+const search = 'latest news'
+
+const searcher = async () => {
+  const browser = await puppeteer.launch({
+    // headless: false,
+    // args: ['--window-size=1280,720'],
+  })
+  const context = await browser.createIncognitoBrowserContext()
+  const page = await context.newPage()
+  await page.setViewport({ width: 1280, height: 720 })
+  await page.goto(`https://duckduckgo.com/?q=${search}&kl=us-en`)
+  await page.waitForNavigation()
+
+  try {
+    const data = await page.$$eval('article>div>h2>a', (results) =>
+      results.map((result) => {
+        return { title: result.textContent, link: result.href }
+      })
+    )
+
+    const results = []
+
+    for (i = 0; i < data.length; i++) {
+      results.push(data[i])
+    }
+
+    console.log(results)
+  } catch {
+    console.log('Something Went Wrong!')
+  }
+
+  await browser.close()
+}
+
+searcher()
